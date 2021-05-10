bc289117fedba7098d1ae2af370d136bf797c06a
sepaivaluna <sepaivaluna@gmail.com>
2021-05-10 17:48:16 -0400

    testing a few things

--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,51 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'sepaivaluna@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  if (isset($_POST['submit'])) {
+    $from_name = $_POST['name'];
+    $from_email = $_POST['mail'];
+    $subject = $_POST['message'];
+    
+    $mailTo = "sepaivaluna@gmail.com";
+    $headers = "From: ".$from_email;
+    $txt = "You have a message ".$name".\n\n".$message;
+
+    mail($mailTo, $name, $txt, $headers);
+    header("Location: index.html?MessageSent");
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+
+
+  // $receiving_email_address = 'sepaivaluna@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // /*
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  // */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
 ?>
